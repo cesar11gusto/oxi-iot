831b7902b72f38500e25d5dcd3698bbbe38cf847
cesar11gusto <68619954+cesar11gusto@users.noreply.github.com>
2020-07-21 20:35:58 -0300

    Initial commit

---
+++ view/contatos.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+
+
+<html lang="port">
+    
+   <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial</title>
+        <!-- Load Roboto font -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <!-- Load css styles -->
+        <link rel="stylesheet" type="text/css" href="codigocss/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/bootstrap-responsive.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/style.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/pluton.css" />
+        <!--[if IE 7]>
+            <link rel="stylesheet" type="text/css" href="css/pluton-ie7.css" />
+        <![endif]-->
+        <link rel="stylesheet" type="text/css" href="codigocss/jquery.cslider.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/jquery.bxslider.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/animate.css" />
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72.png">
+        <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57.png">
+        <link rel="shortcut icon" href="images\Logotipo_IFET.svg.png"><!--icone dabarra de pesquisa-->
+    </head>
+    
+    <body id="index">
+        <div class="navbar">
+            <div class="navbar-inner"> 
+                <div class="container">
+                    <a href="#" >
+                        <img src="images\1.png" width="180" height="60" alt="Logo" /><!--LOGO-->
+                    </a>
+                    <!-- Navigation button, visible on small resolution -->
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-menu"></i>
+                    </button>
+                    <!-- Main navigation -->
+                   
+         
+                    <div class="nav-collapse collapse pull-right">
+                        <ul class="nav" id="top-navigation">
+                            <li><a href="index.php">Página Inicial</a></li>
+                            <li><a href="Quemsomos.php">Quem somos</a></li>
+                            <li><a href="Contatos.php">Contatos</a></li>
+                            <a href="index.php"> <img src="images\LOGO.png" width="120" height="15" ></a>
+
+                       </ul>
+
+                    </div>            
+                </div>
+            </div>
+        </div>
+        <!-- End main navigation -->
+        <!-- Conteudo da Pagina -->
+        <div class="section secondary-section" id="service">
+            <div class="container">
+                <a href="https://mail.google.com/">
+                    <img src="images\gmail.png" class="img-responsive" alt="Imagem responsiva" width="10%\9">
+                    <font size="70"> texto facebook</font>
+                </a>
+            </div>
+            <br>
+            <div class="container">
+                 <a href="https://www.facebook.com/">
+                    <img src="images\face.png" class="img-responsive" alt="Imagem responsiva" width="10%\9">
+                  <font size="70"> texto facebook</font>
+                 </a>
+            </div>
+            <br>
+            <div class="container">
+                <a href="https://www.instagram.com/"><img src="images\insta.png" class="img-responsive" alt="Imagem responsiva" width="10%\9">
+                <font size="70"> texto facebook</font>
+                </a>
+            </div>
+            <br>
+            <div class="container" >
+                <a href="https://web.whatsapp.com/"> 
+                    <img  src="images\whats.png" class="img-responsive" alt="Imagem responsiva" width="10%\9">
+                    <font size="70"> texto facebook</font>
+                </a>
+            </div>
+        </div>
+
+  
+
+<!-- Include javascript -->
+        <script src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery.mixitup.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/modernizr.custom.js"></script>
+        <script type="text/javascript" src="js/jquery.bxslider.js"></script>
+        <script type="text/javascript" src="js/jquery.cslider.js"></script>
+        <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+        <script type="text/javascript" src="js/jquery.inview.js"></script>
+        <!-- Load google maps api and call initializeMap function defined in app.js -->
+        <script async="" defer="" type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initializeMap"></script>
+        <!-- css3-mediaqueries.js for IE8 or older -->
+        <!--[if lt IE 9]>
+            <script src="js/respond.min.js"></script>
+        <![endif]-->
+        <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
---
+++ view/quemsomos.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+
+<html lang="port">
+    
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial</title>
+        <!-- Load Roboto font -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <!-- Load css styles -->
+        <link rel="stylesheet" type="text/css" href="codigocss/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/bootstrap-responsive.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/style.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/pluton.css" />
+        <!--[if IE 7]>
+            <link rel="stylesheet" type="text/css" href="css/pluton-ie7.css" />
+        <![endif]-->
+        <link rel="stylesheet" type="text/css" href="codigocss/jquery.cslider.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/jquery.bxslider.css" />
+        <link rel="stylesheet" type="text/css" href="codigocss/animate.css" />
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72.png">
+        <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57.png">
+        <link rel="shortcut icon" href="images\Logotipo_IFET.svg.png"><!--icone dabarra de pesquisa-->
+    </head>
+    
+    <body id="index">
+        <div class="navbar">
+            <div class="navbar-inner"> 
+                <div class="container">
+                    <a href="#" >
+                        <img src="images\1.png" width="180" height="60" alt="Logo" /><!--LOGO-->
+                    </a>
+                    <!-- Navigation button, visible on small resolution -->
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-menu"></i>
+                    </button>
+                    <!-- Main navigation -->
+                   
+         
+                    <div class="nav-collapse collapse pull-right">
+                        <ul class="nav" id="top-navigation">
+                            <li><a href="index.php">Página Inicial</a></li>
+                            <li><a href="Quemsomos.php">Quem somos</a></li>
+                            <li><a href="Contatos.php">Contatos</a></li>
+                            <a href="index.php"> <img src="images\LOGO.png" width="120" height="15" ></a>
+
+                       </ul>
+
+                    </div>
+                
+
+
+                    <!-- End main navigation -->
+                </div>
+            </div>
+        </div>
+        <!-- Start home section -->
+		<div class="section secondary-section" id="service">
+           <div class="container">
+                <!-- Start title section -->
+
+                    <br><h3 ><p><b>QUEM SOMOS:</b></p></h3><br>
+
+                    <p> 
+                    </p>                   
+
+                    <br><br>
+                    <center>
+                    <h3><b><center>ORIENTADORES:</b></center></h3><br><br>
+                    <div class="row-fluid">
+                            
+                    <div class="span12">
+
+                        <center><b>RICARDO CONDE CAMILLO DA SILVA<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/conde.jpg" alt="service 4"></a>
+                            </div>
+                            <br><br>
+                            <p> 
+                                Desenvolvedor Front-end<br>
+                                Desenvolvedor Back-end<br>
+                                Desenvolvedor BD<br>
+                                Auxilio Monografia<br>
+                                Designer Gráfico<br>
+
+                            </p>
+                        </div>
+                        </div>
+                 </div>
+
+               <br><br><br>
+                    
+                  
+                    <div class="span12">
+                        <center><b>ADRIANO DE SOUZA MARQUES<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a ><img class="img-circle" src="images/adriano.jpg" alt="service 4"></a>
+                            </div>
+                             <br><br>
+                            <p>
+                                Auxilio Monografia<br>
+                                Auxilio Front-end<br>
+                                Auxilio Back-end   
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br><br><br>
+                </CENTER>
+
+                    <h3><b><center>EQUIPE OX-IOT:</center></b></h3><br><br>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <center><b>ANDRÉ CONDE<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/Andre.jpg" alt="service 4"></a>
+                            </div>
+                            <br><br>
+                            <p> 
+                                Desenvolvedor Front-end<br>
+                                Desenvolvedor Back-end<br>
+                                Desenvolvedor BD<br>
+                                Auxilio Monografia<br>
+                                Designer Gráfico<br>
+
+                            </p>
+                        </div>
+
+
+
+                    </div>
+
+                    <!--Cesar Gomes-->
+                    <div class="span4">
+                        <center><b>BRENO MAZZALI<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/breno.jpg" alt="service 4"></a>
+                            </div>
+                             <br><br>
+                            <p>Auxilio Monografia</p>
+                        </div>
+                    </div>
+                    
+                    <!--Henrique-->
+                    <div class="span4">
+                        <center><b>CARLOS HENRIQUE GOMES DOS SANTOS<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a ><img class="img-circle" src="images/CARLOS.jpg" alt="service 4"></a>
+                            </div>
+                             <br><br>
+                            <p>
+                                Auxilio Monografia<br>
+                                Auxilio Front-end<br>
+                                Auxilio Back-end   
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br><br><br>
+                <div class="row-fluid">
+                    <!--Larissa-->
+                    <div class="span4">
+                        <center><b>CESAR AUGUSTO GOMES DOS SANTOS<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/cesar.jpg" alt="service 4"></a>
+                            </div>
+                            <br><br>
+                            <p>Responsável Monografia</p>
+                        </div>
+                    </div>
+                    <!--Layane-->
+                    
+                    <div class="span4">
+                        <center><b>GABRIEL PALMYRO<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/gabriel.jpg" alt="service 4" /></a>
+                            </div>
+                            <br><br>
+                            <p>Auxilio Monografia</p>
+                        </div>
+                    </div>
+
+                    <!--Paulo-->
+                    <div class="span4">
+                        <center><b>GIOVANNA SANTOS FANTACINI<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/giovana.jpg" alt="service 4" /></a>
+                            </div>
+                            <br><br>                            
+                            <p>
+                                Auxilio Monografia<br>
+                                Auxilio Front-end<br>
+                                Auxilio Back-end                                
+                            </p>
+                        </div>
+                    </div>
+                     <br><br><br>
+                <div class="row-fluid">
+                    <!--Larissa-->
+                    <div class="span4">
+                        <center><b>HIGOR GRIGÓRIO<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/higor.jpg" alt="service 4"></a>
+                            </div>
+                            <br><br>
+                            <p>Responsável Monografia</p>
+                        </div>
+                    </div>
+                    <!--Layane-->
+                    
+                    <div class="span4">
+                        <center><b>JÚLIA MOURA<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/JULIA.jpg" alt="service 4" /></a>
+                            </div>
+                            <br><br>
+                            <p>Auxilio Monografia</p>
+                        </div>
+                    </div>
+
+                    <!--Paulo-->
+                    <div class="span4">
+                        <center><b>LEANDRO CAMPOS VARGAS<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a href="novo_jogo.php"><img class="img-circle" src="images/leandro.jpg" alt="service 4" /></a>
+                            </div>
+                            <br><br>                            
+                            <p>
+                                Auxilio Monografia<br>
+                                Auxilio Front-end<br>
+                                Auxilio Back-end                                
+                            </p>
+                        </div>
+                    </div>
+                     <br><br><br>
+                <div class="row-fluid">
+                    <!--Larissa-->
+                    <div class="span4">
+                        <center><b>LEONARDO FREDERICO<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/leo.jpg" alt="service 4"></a>
+                            </div>
+                            <br><br>
+                            <p>Responsável Monografia</p>
+                        </div>
+                    </div>
+                    <!--Layane-->
+                    
+                    <div class="span4">
+                        <center><b>LUCAS ANDREI<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a><img class="img-circle" src="images/lucas.jpg" alt="service 4" /></a>
+                            </div>
+                            <br><br>
+                            <p>Auxilio Monografia</p>
+                        </div>
+                    </div>
+
+                    <!--Paulo-->
+                    <div class="span4">
+                        <center><b>NATÁLIA BIANCA PUGLIA CONDE<b></center>
+                        <div class="centered service">
+                            <div class="circle-border zoom-in">
+                                <a href="novo_jogo.php"><img class="img-circle" src="images/.jpg" alt="service 4" /></a>
+                            </div>
+                            <br><br>                            
+                            <p>
+                                Auxilio Monografia<br>
+                                Auxilio Front-end<br>
+                                Auxilio Back-end                                
+                            </p>
+                        </div>
+                    </div>
+                      <center><img src="images\LOGO.png" width="80%" height="80%" alt="Logo" /><br><br></center>
+                </div>
+            </div>
+        </div>
+        
+
+
+        <!-- Include javascript -->
+        <script src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery.mixitup.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/modernizr.custom.js"></script>
+        <script type="text/javascript" src="js/jquery.bxslider.js"></script>
+        <script type="text/javascript" src="js/jquery.cslider.js"></script>
+        <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+        <script type="text/javascript" src="js/jquery.inview.js"></script>
+        <!-- Load google maps api and call initializeMap function defined in app.js -->
+        <script async="" defer="" type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initializeMap"></script>
+        <!-- css3-mediaqueries.js for IE8 or older -->
+        <!--[if lt IE 9]>
+            <script src="js/respond.min.js"></script>
+        <![endif]-->
+        <script type="text/javascript" src="js/app.js"></script>
+    </body>
+</html>
+
+ 
+                    
+              
